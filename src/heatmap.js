eb47cb2f1dbe2606fd2b92c46bc72577acd6b4e0
mixayloff-dimaaylov <65488726+mixayloff-dimaaylov@users.noreply.github.com>
2022-01-05 15:39:30 +0300

    Update level colors

--- src/heatmap.js
+++ src/heatmap.js
@@ -24,7 +24,7 @@
 var HeatmapConfig = {
   defaultRadius: 40,
   defaultRenderer: 'canvas2d',
-  defaultGradient: { 0.25: "rgb(0,0,255)", 0.55: "rgb(0,255,0)", 0.85: "yellow", 1.0: "rgb(255,0,0)"},
+  defaultGradient: { 0.01: "rgb(0,0,255)", 0.20: "rgb(0,255,0)", 0.55: "yellow", 0.75: "rgb(255,0,0)"},
   defaultMaxOpacity: 1,
   defaultMinOpacity: 0,
   defaultBlur: .85,
